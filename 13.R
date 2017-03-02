745ffccb7f99c43b86cfe2a4bd9041d721c2fd2d
knagaev <knagaev@gmail.com>
2017-03-02 10:55:00 +0300

    evil join

--- 13.R
+++ 13.R
@@ -1,12 +1,12 @@
 
 ### 1.Compute the average delay by destination, then join on the airports data frame so you can show the spatial distribution of delays. Here’s an easy way to draw a map of the United States:
   
-  airports %>%
-  semi_join(flights %>%
-  ggplot(aes(lon, lat)) +
-  borders("state") +
-  geom_point() +
-  coord_quickmap()
+#airports %>%
+#  semi_join(flights %>%
+#  ggplot(aes(lon, lat)) +
+#  borders("state") +
+#  geom_point() +
+#  coord_quickmap()
 #(Don’t worry if you don’t understand what semi_join() does — you’ll learn about it next.)
 #You might want to use the size or colour of the points to display the average delay for each airport.
 
@@ -27,9 +27,24 @@ delays %>%
 ### 2.Add the location of the origin and destination (i.e. the lat and lon) to flights.
 flights %>%
   inner_join(airports, c("dest" = "faa")) %>% 
-  rename(dest_lat = lat, dest_lon = lon) %>% 
-  inner_join(airports, c("origin" = "faa")) %>% 
-  rename(origin_lat = lat, origin_lon = lon)
+    rename(dest_lat = lat, dest_lon = lon) %>% 
+  inner_join(airports, c("origin" = "faa")) #%>% 
+    rename(origin_lat = lat, origin_lon = lon)
 
-               
+dest <-    
+flights %>%
+  inner_join(airports, by = c("dest" = "faa"))
 
+res <-
+dest %>% 
+  inner_join(airports, c("origin" = "faa"), suffix = c("_dest", "_origin"))
+
+res <-
+  flights %>%
+  inner_join(airports, ("dest" = "faa")) %>% 
+  inner_join(airports, by = cc("origin" = "faa"), suffix = c("_dest", "_origin"))
+
+# evil
+#flights %>%
+#  inner_join(airports, c("dest" = "faa"), suffix = c("", "_dest")) %>% 
+#  inner_join(airports, c("origin" = "faa"), suffix = c("", "_origin"))
